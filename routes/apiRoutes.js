d4cb654765d4f1e6b0decc8b4e723e4ffb3b7493
toon <nu.chai56@gmail.com>
2020-04-28 01:28:27 -0500

    apiRoutes works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,45 @@
 const Workout = require('../models/workout')
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.post('/api/workout', ({body},res)=>{
-        Workout.create(body)
-        .then(dbWorkout =>{
-            res.json(dbWorkout)
-        }).catch(err=>{
-            res.json(err)
+
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({})
+          .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
         })
+        app.put("/api/workouts/:id", ({ body, params }, res) => {
+            Workout.findByIdAndUpdate(
+                params.id,
+                { $push: { exercises: body } },
+                { new: true, runValidators: true }
+            )
+                .then(data => res.json(data))
+                .catch(err => {
+                    console.log("err", err)
+                    res.json(err)
+                })
+        });
+    app.post('/api/workouts', ({ body }, res) => {
+        Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            }).catch(err => {
+                res.json(err)
+            })
     })
-    app.get('/api/workout',(req,res)=>{
-    Workout.find({})
-
-    .then(dbWorkout =>{
-        res.json(dbWorkout)
-    }).catch(err=>{
-        res.json(err)
-    })
-})
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find({})
+          .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+        })
+    
 }
